16c63d9518b5eb73c20f2d3713de57dbd8199dca
julianamm <julianamm88@gmail.com>
2022-10-21 15:19:43 -0700

    Add home layout structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,30 @@
-import { styled } from '../styles'
+import Image from "next/image"
+import { HomeContainer, Product } from "../styles/pages/home"
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 8,
-  border: 0,
-  padding: '0.25rem 0.5rem',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'Brightness(0.8)',
-  }
-})
+import shirt1 from '../assets/shirt_01.png' 
+import shirt2 from '../assets/shirt_02.png' 
+import shirt3 from '../assets/shirt_03.png' 
+import shirt4 from '../assets/shirt_04.png'
 
 export default function Home() {
   return (
-      <Button>hello world
-        <span>test</span>
-      </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Shirt X</strong>
+          <span>$79.99</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt='' />
+
+        <footer>
+          <strong>Shirt X</strong>
+          <span>$79.99</span>
+        </footer>
+      </Product>
+  </HomeContainer>
   )
 }
